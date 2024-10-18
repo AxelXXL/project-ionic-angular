cf21e24b0172956eb55d8fd88761ddf21df0123a
AxelXXL <axelolmar@gmail.com>
2024-10-18 00:11:05 -0600

    Ajuste al diseño y creacion del componente para la lista de las peliculas

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -2,15 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { IonicModule } from '@ionic/angular';
+import { ListPelisComponent } from './list-pelis/list-pelis.component';
 
 
 
 @NgModule({
   declarations: [ 
-    HeaderComponent
+    HeaderComponent,
+    ListPelisComponent
   ],
   exports:[
-    HeaderComponent
+    HeaderComponent,
+    ListPelisComponent
   ],
   imports: [
     CommonModule,
--- src/app/components/list-pelis/list-pelis.component.ts
+++ src/app/components/list-pelis/list-pelis.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import axios from 'axios';
 
 @Component({
   selector: 'app-list-pelis',
@@ -9,6 +10,26 @@ export class ListPelisComponent  implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {}
+  pelisList = [];
+
+  async ngOnInit() {
+    const options = {
+      method: 'GET',
+      url: 'https://imdb-top-100-movies.p.rapidapi.com/',
+      headers: {
+        'x-rapidapi-key': '32b50a9f4dmsh59022422fe7b514p1f35b1jsn08ce94ba2bb5',
+        'x-rapidapi-host': 'imdb-top-100-movies.p.rapidapi.com'
+      }
+    };
+    
+    try {
+      const response = await axios.request(options);
+      this.pelisList = response.data;
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+
+  }
 
 }
