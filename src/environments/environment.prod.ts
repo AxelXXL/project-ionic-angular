b8e5861b03bcea3c36996b265e86c615fb14f11a
AxelXXL <axelolmar@gmail.com>
2024-11-11 23:19:03 -0600

    configuraciones para que funcione el service worker

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  rapidApiKey: '32b50a9f4dmsh59022422fe7b514p1f35b1jsn08ce94ba2bb5',
+  rapidApiHost: 'imdb-top-100-movies.p.rapidapi.com'
 };
